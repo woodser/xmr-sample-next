033d012f318fdd811fc8b7ad1db9a7e491a674d9
mainnet-pat <mainnet_pat@protonmail.com>
2024-04-19 11:33:33 +0000

    More boilerplate code for proper worker instantiation and fs polyfill

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,28 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-            config.externals["web-worker"] = "web-worker";
-        }
-        return config;
+  webpack: (config, { isServer }) => {
+    config.experiments = { ...config.experiments, topLevelAwait: true }
+
+    if (!isServer) {
+      config.resolve.alias.fs = "indexeddb-fs";
+      config.resolve.fallback.child_process = false;
+    }
+
+    config.externals = [...config.externals, ({ request }, callback) => {
+      if (/^web-worker$/.test(request)) {
+        return callback(null, request);
       }
+      callback();
+    }];
+
+
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { createWalletFull, MoneroNetworkType } from "monero-ts";
 
 export default function Home() {
+  const [address, setAddress] = useState("loading...");
+  useEffect(() => {
+    (async () => {
+      const wallet = await createWalletFull({
+        server: "http://localhost:18081",
+        path: "test_wallet",
+        networkType: MoneroNetworkType.MAINNET,
+      });
+
+      setAddress(await wallet.getPrimaryAddress());
+    })();
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -8,6 +25,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
+        <span>Wallet address: {address}</span>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
