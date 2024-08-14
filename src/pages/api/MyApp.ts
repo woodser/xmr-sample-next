20f759601f8f9ba301abcc00c474f977d9063a93
woodser <woodser@protonmail.com>
2024-08-14 11:18:14 -0400

    update sample app

--- src/pages/api/MyApp.ts
+++ src/pages/api/MyApp.ts
@@ -1,36 +1,77 @@
+import assert from 'assert';
 import moneroTs from "monero-ts";
 
 const myApp = async () => {
 
-    // connect to daemon rpc
-    console.log("Connecting to daemon RPC");
-    let daemonRpc = await moneroTs.connectToDaemonRpc({
-      uri: "http://localhost:28081",
-      username: "rpc_user",
-      password: "abc123",
-      rejectUnauthorized: false
-    });
-    console.log("Daemon RPC height: " + await daemonRpc.getHeight());
-
-    // connect to wallet RPC
-    console.log("Connecting to wallet RPC");
-    let walletRpc = await moneroTs.connectToWalletRpc({
-      uri: "http://localhost:28084",
-      username: "rpc_user",
-      password: "abc123",
-      rejectUnauthorized: false
-    });
-    //await walletRpc.openWallet("test_wallet_1", "supersecretpassword123");
-    console.log("Wallet RPC seed: " + await walletRpc.getSeed());
-
-    // create offline full wallet
+    // connect to a daemon
+    console.log("Connecting to daemon");
+    let daemon = await moneroTs.connectToDaemonRpc("http://localhost:28081");
+    let height = await daemon.getHeight();            // 1523651
+    let feeEstimate = await daemon.getFeeEstimate();  // 1014313512
+    let txsInPool = await daemon.getTxPool();         // get transactions in the pool
+    
+    // create wallet from seed phrase using WebAssembly bindings to monero-project
+    console.log("Creating wallet from seed phrase");
     let walletFull = await moneroTs.createWalletFull({
-      //path: GenUtils.getUUID(), // in-memory wallet if not given
       password: "supersecretpassword123",
-      networkType: moneroTs.MoneroNetworkType.STAGENET,
+      networkType: moneroTs.MoneroNetworkType.TESTNET,
+      seed: "silk mocked cucumber lettuce hope adrenalin aching lush roles fuel revamp baptism wrist long tender teardrop midst pastry pigment equip frying inbound pinched ravine frying",
+      restoreHeight: 171,
+      server: {
+        uri: "http://localhost:28081",
+        username: "superuser",
+        password: "abctesting123"
+      }
     });
-    console.log("Created full wallet with seed: " + await walletFull.getSeed());
+    
+    // synchronize with progress notifications
+    console.log("Synchronizing wallet");
+    await walletFull.sync(new class extends moneroTs.MoneroWalletListener {
+      async onSyncProgress(height: number, startHeight: number, endHeight: number, percentDone: number, message: string) {
+        // feed a progress bar?
+      }
+    });
+    
+    // synchronize in the background
+    await walletFull.startSyncing(5000);
+    
+    // listen for incoming transfers
+    let fundsReceived = false;
+    await walletFull.addListener(new class extends moneroTs.MoneroWalletListener {
+      async onOutputReceived(output: moneroTs.MoneroOutputWallet) {
+        let amount = output.getAmount();
+        let txHash = output.getTx().getHash();
+        fundsReceived = true;
+      }
+    });
 
+    // open wallet on monero-wallet-rpc
+    console.log("Opening monero-wallet-rpc");
+    let walletRpc = await moneroTs.connectToWalletRpc("http://localhost:28084", "rpc_user", "abc123");
+    await walletRpc.openWallet("test_wallet_1", "supersecretpassword123");
+    let primaryAddress = await walletRpc.getPrimaryAddress(); // 555zgduFhmKd2o8rPUz...
+    let balance = await walletRpc.getBalance();               // 533648366742
+    let txs = await walletRpc.getTxs();                       // get transactions containing transfers to/from the wallet
+
+    // send funds from RPC wallet to WebAssembly wallet
+    console.log("Transferring funds from monero-wallet-rpc");
+    let createdTx = await walletRpc.createTx({
+      accountIndex: 0,
+      address: await walletFull.getAddress(1, 0),
+      amount: 5000000n, // amount to transfer in atomic units
+      relay: false // create transaction and relay to the network if true
+    });
+    let fee = createdTx.getFee(); // "Are you sure you want to send... ?"
+    await walletRpc.relayTx(createdTx); // relay the transaction
+    
+    // recipient receives unconfirmed funds within 5s seconds
+    await new Promise(function(resolve) { setTimeout(resolve, 5000); });
+    assert(fundsReceived);
+    
+    // close wallets
+    console.log("Closing wallets");
+    await walletFull.close();
+    await walletRpc.close();
     console.log("Done running XMR sample app");
     return true;
 }
