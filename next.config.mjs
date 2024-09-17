150c9d347401bb840d95f8d5dadb6ded40bfe5b8
mainnet-pat <mainnet_pat@protonmail.com>
2024-09-17 19:26:02 +0000

    Demonstate the workflow in frontend
    Co-authored-by: mainnet-pat <mainnet_pat@protonmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-            config.externals["web-worker"] = "web-worker";
-        }
-        return config;
+  webpack: (config, { isServer }) => {
+    config.experiments = { ...config.experiments, topLevelAwait: true }
+
+    if (!isServer) {
+      config.resolve.alias.fs = "memfs";
+      config.resolve.fallback.child_process = false;
+    }
+
+    config.externals = [...config.externals, ({ request }, callback) => {
+      if (/^web-worker$/.test(request)) {
+        return callback(null, request);
       }
+      callback();
+    }];
+
+    return config;
+  },
 };
 
 export default nextConfig;
